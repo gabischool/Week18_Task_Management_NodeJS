cb097bba12512769142228f8806b5b3c4c0e424f
Mohamud E Hasen <mohmudes4@gmail.com>
2025-07-29 12:55:21 -0500

    added week18 assignment

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json()); // Parse JSON request bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
-
 // Mount API routes
 app.use("/api", taskRoutes);
 
